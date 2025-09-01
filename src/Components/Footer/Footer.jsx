16b3abc29c2042cdadbbcc4920598cc97596c414
Matheus Pontes <matheuspontes5@hotmail.com>
2025-08-31 21:41:08 -0300

    teste

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 
     return (
         <div className="bg-roxoSecundario w-full items-center text-center text-white xl:text-[20px] text-[12px] py-[40px]">
-                <p>© 2025 A Cozinha da Jane - Todos os direitos reservados.</p>
+                <p>© 2025 A Cozinha da Jane - Todos os direitos res.</p>
             </div>
     )
 }
